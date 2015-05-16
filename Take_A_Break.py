4744f83bdac37349fe5d266190386567230619b0
Jason Cooke <Jason_Cooke@server.fake>
2015-05-16 14:51:38 +1200

    First commit

---
+++ Take_A_Break.py
@@ -0,0 +1,15 @@
+# The point of this program is to open a new browser window every hour to notify the user that is time to take a break.
+# time.sleep takes a argument of seconds.
+
+import time
+import webbrowser
+
+myArray = [r"https://www.youtube.com/watch?v=_TcB3SBDVGY", r"https://www.youtube.com/watch?v=lRe9UmpOWys", r"https://www.youtube.com/watch?v=jZoTLP8O1qg"]
+
+def myFunction():
+    for song in range(0, 3):
+        time.sleep(60 * 60)
+        webbrowser.open(myArray[song])
+        print(myArray[song])
+
+myFunction()
