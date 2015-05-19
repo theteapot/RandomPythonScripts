df7ec015c2295e8f0bcb1eed903325808d3033d0
Jason Cooke <Jason_Cooke@server.fake>
2015-05-19 19:39:12 +1200

    First commit

---
+++ check_for_curse_words.py
@@ -0,0 +1,16 @@
+import urllib
+
+def readTextFromFile():
+    file = open("")     # put your file path inbetween the quotation marks.
+    contentsOfFile = file.read()
+    print(contentsOfFile)
+    file.close()
+    checkForCurseWords(contentsOfFile)
+
+def checkForCurseWords(textInput):
+    url = urllib.urlopen("http://www.wdyl.com/profanity?q=" + textInput)
+    result = url.read()
+    print(result)
+    url.close()
+
+readTextFromFile()
