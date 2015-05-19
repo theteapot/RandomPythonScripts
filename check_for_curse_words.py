0008047929d878e416d9646f199d9c97b6a0da8c
Jason Cooke <Jason-Cooke@users.noreply.github.com>
2015-05-19 19:43:40 +1200

    Add comment

--- check_for_curse_words.py
+++ check_for_curse_words.py
@@ -6,9 +6,9 @@ def readTextFromFile():
     print(contentsOfFile)
     file.close()
     checkForCurseWords(contentsOfFile)
-
+    
 def checkForCurseWords(textInput):
-    url = urllib.urlopen("http://www.wdyl.com/profanity?q=" + textInput)
+    url = urllib.urlopen("http://www.wdyl.com/profanity?q=" + textInput) # Uses Googles what do you love website to test for profantiy
     result = url.read()
     print(result)
     url.close()
