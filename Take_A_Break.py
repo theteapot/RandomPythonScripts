ce1bc96d9702d530013c7ad7cfe319be5105faac
theteapot <tin.teapot@gmail.com>
2017-10-27 12:58:09 +1300

    Update Take_A_Break.py
    added some comments, got rid of raw strings

--- Take_A_Break.py
+++ Take_A_Break.py
@@ -1,15 +1,15 @@
 # The point of this program is to open a new browser window every hour to notify the user that is time to take a break.
-# time.sleep takes an argument of seconds.
+# It will also open a web browser that will play specified song
 
 import time
 import webbrowser
 
-myArray = [r"https://www.youtube.com/watch?v=_TcB3SBDVGY", r"https://www.youtube.com/watch?v=lRe9UmpOWys", r"https://www.youtube.com/watch?v=jZoTLP8O1qg"]
+songArray = ["https://www.youtube.com/watch?v=_TcB3SBDVGY", "https://www.youtube.com/watch?v=lRe9UmpOWys", "https://www.youtube.com/watch?v=jZoTLP8O1qg"]
 
-def myFunction():
-    for song in range(0, 3):
+def main():
+    for song in songArray:
         time.sleep(60 * 60)
-        webbrowser.open(myArray[song])
-        print(myArray[song])
+        webbrowser.open(song)
+        print(song)
 
-myFunction()
+main()
